71ef8521acc79573eedbd5261c5f21bcd33cc4b0
unknown <raduran8080@gmail.com>
2018-10-18 19:15:38 -0500

    arreglos

--- src/js/app.js
+++ src/js/app.js
@@ -1,50 +1,108 @@
-// funcion 
+Imprime de 1 a 10000
+for (var i = 1; i <= 10000; i++) {
+    console.log(i);
+}
 
-// function sumar() {
-//   var num1 = document.getElementById('numero1').value;
-//   var num2 = document.getElementById('numero2').value;
-//   var res = document.getElementById('resultado');
+Imprime numeros pares del 0 al 10000
+i += 2
+for (var i = 0; i <= 10000; i = i + 2) {
+    console.log(i);
+}
 
-//   var resultado  = Number(num1) + Number(num2);
-//   res.innerHTML = resultado;
-
-//   return resultado;
-// }
-
-function esExacta(num1, num2) {
-    if ((num1 % num2) === 0) {
-        return true;
-    } else {
-        return false;
+for (var i = 0; i <= 10000; i++) {
+    if (i % 2 === 0) {
+        console.log(i);
     }
 }
 
-function calculaAnios(yearNow, otherYear) {
-    if (yearNow > otherYear) {
-        var resultado = yearNow - otherYear;
-        return ("Han pasado " + resultado)
-    } else if (yearNow < otherYear) {
-        var resultado = otherYear - yearNow;
-        return ("Faltan " + resultado + " años.")
+Suma de numeros pares del 1 al 10000
+var suma = 0;
+for (var i = 1; i <= 10000; i++) {
+    if (i % 2 == 0) {
+        suma = suma + i;
+    }
+}
+console.log(suma);
+
+Crear la serie de Fibonacci
+var fibonacci = 0;
+for (var i = 0; i <= 5; i++) {
+    var aSumar = i + fibonacci;
+    fibonacci = fibonacci + aSumar;
+    console.log(fibonacci)
+}
+console.log("Resultado", fibonacci);
+
+function fibonacci(num) {
+    var a = 1
+    var b = 0
+    var temp;
+
+    while (num >= 0) {
+        temp = a;
+        a = a + b;
+        b = temp;
+        num--
+        console.log(b);
+    }
+}
+
+FAIL
+
+function fibonacci(num) {
+    if (num <= 1) {
+        return 1
     } else {
-        return ("Es el mismo año");
+        return fibonacci(num - 1) + fibonacci(num - 2);
+    }
+}
+console.log(fibonacci(100))
+
+var carro = {
+    modelo: "2018",
+    marca: "VW",
+    encendido: false,
+    arrancar: function(llave) {
+        if (llave === "123") {
+            this.encendido = true;
+        } else {
+            console.log("Llave incorrecta");
+        }
+    }
+}
+
+var objeto1 = {
+    valor1: 34,
+    valor2: "Hola",
+    suma: function(num1, num2) {
+        return num1 + num2;
+    },
+    objetoInterno: {
+        valorInterno: 34,
+        valorInterno2: "Adios",
     }
 }
 
 
-function programa1() {
-    var numero1 = prompt("Ingresa el valor del primer número: ");
-    var numero2 = prompt("Ingresa el valoer del segundo número: ");
-    var resultado = esExacta(numero1, numero2)
-    if (resultado) {
-        alert("La división es exacta.")
-    } else {
-        alert("La división no es exacta.")
-    }
+var misa = {
+    edad: 21,
+    sexo: "Masculino",
+    pais: "Mexico"
 }
 
-function programa3() {
-    var yearNow = Number(prompt("Ingresa el año actual."));
-    var otherYear = Number(prompt("Ingresa cualquier otro año. "));
-    alert(calculaAnios(yearNow, otherYear))
+Arrays
+var array = [];
+for (var i = 1; i <= 100; i++) {
+    array.push(i);
+}
+console.log(array);
+
+var fabrica = {
+    arrayDeColores: ["rojo", "azul", "verde", "violeta", "gris"],
+
+    creaCoches: function() {
+        for (var i = 0; i < this.arrayDeColores.length; i++) {
+            console.log("El carro " + this.arrayDeColores[i])
+        }
+    }
 }
